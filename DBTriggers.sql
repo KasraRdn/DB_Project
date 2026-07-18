dbeeb62d339bd299245f52b295ac3885cdbc15a9
KasraRdn <kasa1384.radan1384@gmail.com>
2026-07-18 12:05:33 +0330

    Update tables and triggers codes

--- DBTriggers.sql
+++ DBTriggers.sql
@@ -20,7 +20,7 @@ BEGIN
     END
 END;
 
-CREATE OR ALTER TRIGGER Food.LogUsers ON Users AFTER INSERT, UPDATE, DELETE AS
+CREATE OR ALTER TRIGGER Food.LogUsers ON Food.Users AFTER INSERT, UPDATE, DELETE AS
 BEGIN
     DECLARE @Action VARCHAR(10);
     IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted) SET @Action = 'UPDATE';
@@ -29,7 +29,7 @@ BEGIN
     INSERT INTO Food.SystemLogs (TableName, Action, Description) VALUES ('Users', @Action, 'Modification in Users table.');
 END;
 
-CREATE OR ALTER TRIGGER Food.LogRestaurants ON Restaurants AFTER INSERT, UPDATE, DELETE AS
+CREATE OR ALTER TRIGGER Food.LogRestaurants ON Food.Restaurants AFTER INSERT, UPDATE, DELETE AS
 BEGIN
     DECLARE @Action VARCHAR(10);
     IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted) SET @Action = 'UPDATE';
@@ -38,7 +38,7 @@ BEGIN
     INSERT INTO Food.SystemLogs (TableName, Action, Description) VALUES ('Restaurants', @Action, 'Modification in Restaurants table.');
 END;
 
-CREATE OR ALTER TRIGGER Food.LogCategories ON Categories AFTER INSERT, UPDATE, DELETE AS
+CREATE OR ALTER TRIGGER Food.LogCategories ON Food.Categories AFTER INSERT, UPDATE, DELETE AS
 BEGIN
     DECLARE @Action VARCHAR(10);
     IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted) SET @Action = 'UPDATE';
@@ -47,7 +47,7 @@ BEGIN
     INSERT INTO Food.SystemLogs (TableName, Action, Description) VALUES ('Categories', @Action, 'Modification in Categories table.');
 END;
 
-CREATE OR ALTER TRIGGER Food.LogCoupons ON Coupons AFTER INSERT, UPDATE, DELETE AS
+CREATE OR ALTER TRIGGER Food.LogCoupons ON Food.Coupons AFTER INSERT, UPDATE, DELETE AS
 BEGIN
     DECLARE @Action VARCHAR(10);
     IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted) SET @Action = 'UPDATE';
@@ -56,7 +56,7 @@ BEGIN
     INSERT INTO Food.SystemLogs (TableName, Action, Description) VALUES ('Coupons', @Action, 'Modification in Coupons table.');
 END;
 
-CREATE OR ALTER TRIGGER Food.LogAddresses ON Addresses AFTER INSERT, UPDATE, DELETE AS
+CREATE OR ALTER TRIGGER Food.LogAddresses ON Food.Addresses AFTER INSERT, UPDATE, DELETE AS
 BEGIN
     DECLARE @Action VARCHAR(10);
     IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted) SET @Action = 'UPDATE';
@@ -65,7 +65,7 @@ BEGIN
     INSERT INTO Food.SystemLogs (TableName, Action, Description) VALUES ('Addresses', @Action, 'Modification in Addresses table.');
 END;
 
-CREATE OR ALTER TRIGGER Food.LogMenuItems ON MenuItems AFTER INSERT, UPDATE, DELETE AS
+CREATE OR ALTER TRIGGER Food.LogMenuItems ON Food.MenuItems AFTER INSERT, UPDATE, DELETE AS
 BEGIN
     DECLARE @Action VARCHAR(10);
     IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted) SET @Action = 'UPDATE';
@@ -74,7 +74,7 @@ BEGIN
     INSERT INTO Food.SystemLogs (TableName, Action, Description) VALUES ('MenuItems', @Action, 'Modification in MenuItems table.');
 END;
 
-CREATE OR ALTER TRIGGER Food.LogReviews ON Reviews AFTER INSERT, UPDATE, DELETE AS
+CREATE OR ALTER TRIGGER Food.LogReviews ON Food.Reviews AFTER INSERT, UPDATE, DELETE AS
 BEGIN
     DECLARE @Action VARCHAR(10);
     IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted) SET @Action = 'UPDATE';
@@ -83,7 +83,7 @@ BEGIN
     INSERT INTO Food.SystemLogs (TableName, Action, Description) VALUES ('Reviews', @Action, 'Modification in Reviews table.');
 END;
 
-CREATE OR ALTER TRIGGER Food.LogOrders ON Orders AFTER INSERT, UPDATE, DELETE AS
+CREATE OR ALTER TRIGGER Food.LogOrders ON Food.Orders AFTER INSERT, UPDATE, DELETE AS
 BEGIN
     DECLARE @Action VARCHAR(10);
     IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted) SET @Action = 'UPDATE';
@@ -92,7 +92,7 @@ BEGIN
     INSERT INTO Food.SystemLogs (TableName, Action, Description) VALUES ('Orders', @Action, 'Modification in Orders table.');
 END;
 
-CREATE OR ALTER TRIGGER Food.LogOrderDetails ON OrderDetails AFTER INSERT, UPDATE, DELETE AS
+CREATE OR ALTER TRIGGER Food.LogOrderDetails ON Food.OrderDetails AFTER INSERT, UPDATE, DELETE AS
 BEGIN
     DECLARE @Action VARCHAR(10);
     IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted) SET @Action = 'UPDATE';
@@ -101,7 +101,7 @@ BEGIN
     INSERT INTO Food.SystemLogs (TableName, Action, Description) VALUES ('OrderDetails', @Action, 'Modification in OrderDetails table.');
 END;
 
-CREATE OR ALTER TRIGGER Food.LogOrderStatusHistory ON OrderStatusHistory AFTER INSERT, UPDATE, DELETE AS
+CREATE OR ALTER TRIGGER Food.LogOrderStatusHistory ON Food.OrderStatusHistory AFTER INSERT, UPDATE, DELETE AS
 BEGIN
     DECLARE @Action VARCHAR(10);
     IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted) SET @Action = 'UPDATE';
@@ -110,7 +110,7 @@ BEGIN
     INSERT INTO Food.SystemLogs (TableName, Action, Description) VALUES ('OrderStatusHistory', @Action, 'Modification in OrderStatusHistory table.');
 END;
 
-CREATE OR ALTER TRIGGER Food.LogPayments ON Payments AFTER INSERT, UPDATE, DELETE AS
+CREATE OR ALTER TRIGGER Food.LogPayments ON Food.Payments AFTER INSERT, UPDATE, DELETE AS
 BEGIN
     DECLARE @Action VARCHAR(10);
     IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted) SET @Action = 'UPDATE';
